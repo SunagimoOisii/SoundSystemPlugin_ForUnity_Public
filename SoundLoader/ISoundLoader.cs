ab87796b06235a54b7449613d2303506eed29067
Fukada_Ryuto <144636715+SunagimoOisii@users.noreply.github.com>
2025-05-12 17:21:01 +0900

    Delete SoundLoader directory

--- SoundLoader/ISoundLoader.cs
+++
@@ -1,9 +0,0 @@
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-
-public interface ISoundLoader
-{
-    UniTask<(bool success, AudioClip clip)> TryLoadClip(string resourceAddress);
-
-    void UnloadClip(string resourceAddress);
-}
